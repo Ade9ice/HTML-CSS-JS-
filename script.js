034ebac92bbf6b3752a38828601b29286aec4b9a
Ade9ice <54281933+Ade9ice@users.noreply.github.com>
2020-03-26 13:16:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+let name = "Adenrele Cuduz" //Name stored in a variable
+
+const courses= ["HTML","CSS", "JAVASCRIPT"]; //Courses stored in an array
+
+//Displaying odd numbers from 1-200
+for (var i = 1; i <= 200; i++) {
+	if (i % 2 !== 0) console.log(i);
+}
+console.log(name);
+console.log(courses);
+
+
+
+
